401644b719e11dbb2e6745d68a0314f527326f4d
alexkmbk <alexkmbkdr1@gmail.com>
2016-03-04 23:40:10 +0600

    Вместо использования констант, реализована передача путей через параметры командной строки

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <winsock2.h>
 #include <stdio.h>
 #include <windows.h>
+#include <string>
 
 // Need to link with Ws2_32.lib
 #pragma comment(lib, "ws2_32.lib")
 
-const WCHAR pServiceName[] = L"KVPNCSvc"; // Имя службы Керио
-const WCHAR pPathToRDP[] = L"mstsc D:\\bin\\scripts\\test.rdp"; // Путь к файлу настроек подключения RDP
-const WCHAR pPathToVPN[] = L"D:\\bin\\KerioVPNClient\\VPN Client\\kvpncgui.exe"; //Путь к клиенту
-const char pServerName[] = "server"; // Имя удаленного сервера, к которому идет подключение
-
+using namespace std;
 
 WCHAR* GetErrorInf()
 {
@@ -23,10 +20,10 @@ WCHAR* GetErrorInf()
 		// use system message tables to retrieve error text
 		FORMAT_MESSAGE_FROM_SYSTEM
 		// allocate buffer on local heap for error text
-		|FORMAT_MESSAGE_ALLOCATE_BUFFER
+		| FORMAT_MESSAGE_ALLOCATE_BUFFER
 		// Important! will fail otherwise, since we're not 
 		// (and CANNOT) pass insertion parameters
-		|FORMAT_MESSAGE_IGNORE_INSERTS,  
+		| FORMAT_MESSAGE_IGNORE_INSERTS,
 		NULL,    // unused with FORMAT_MESSAGE_FROM_SYSTEM
 		lErrorCode,
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
@@ -34,7 +31,7 @@ WCHAR* GetErrorInf()
 		0, // minimum size for output buffer
 		NULL);   // arguments - see note 
 
-	if ( NULL != errorText )
+	if (NULL != errorText)
 	{
 		// Выделим память по количеству символов в строке
 		int errorTextLen = lstrlenW(errorText);
@@ -49,14 +46,14 @@ WCHAR* GetErrorInf()
 
 }
 
-bool isServiceStarted(WCHAR* name)
+bool isServiceStarted(const WCHAR* name)
 {
-	SC_HANDLE serviceDbHandle = OpenSCManager(NULL,NULL,GENERIC_READ);
+	SC_HANDLE serviceDbHandle = OpenSCManager(NULL, NULL, GENERIC_READ);
 	SC_HANDLE serviceHandle = OpenService(serviceDbHandle, name, GENERIC_READ);
 
 	SERVICE_STATUS_PROCESS status;
 	DWORD bytesNeeded;
-	QueryServiceStatusEx(serviceHandle, SC_STATUS_PROCESS_INFO,(LPBYTE) &status,sizeof(SERVICE_STATUS_PROCESS), &bytesNeeded);
+	QueryServiceStatusEx(serviceHandle, SC_STATUS_PROCESS_INFO, (LPBYTE)&status, sizeof(SERVICE_STATUS_PROCESS), &bytesNeeded);
 
 	if (status.dwCurrentState == SERVICE_RUNNING)
 	{
@@ -71,17 +68,17 @@ bool isServiceStarted(WCHAR* name)
 	return false;
 }
 
-bool StartServiceEasy(WCHAR* name)
+bool StartServiceEasy(const WCHAR* name)
 {
 
-    if (isServiceStarted(name))
+	if (isServiceStarted(name))
 	{
 		return true;
 	}
 
 	SC_HANDLE  hSCManager = OpenSCManager(NULL, NULL, SERVICE_START);
 
-	if (!hSCManager) 
+	if (!hSCManager)
 	{
 		WCHAR* wcErrorInf = GetErrorInf();
 		if (wcErrorInf != NULL) {
@@ -93,22 +90,21 @@ bool StartServiceEasy(WCHAR* name)
 
 	SC_HANDLE hService = OpenService(hSCManager, name, SERVICE_START);
 
-	if (!hService) 
+	if (!hService)
 	{
 		WCHAR* wcErrorInf = GetErrorInf();
 		if (wcErrorInf != NULL) {
-			//MessageBox(NULL,wcErrorInf, L"OpenService error", 0);
 			delete[] wcErrorInf;
 		}
-		CloseServiceHandle (hSCManager);
+		CloseServiceHandle(hSCManager);
 		return false;
 	}
 
 	if (!StartService(hService, 0, NULL)) {
 		if (GetLastError() != ERROR_SERVICE_ALREADY_RUNNING) {
 			//MessageBox(0,L"The service \"KVPNCSvc\" could not be started...", 0, 0);
-			CloseServiceHandle (hSCManager);
-			CloseServiceHandle (hService);
+			CloseServiceHandle(hSCManager);
+			CloseServiceHandle(hService);
 			return false;
 		}
 	};
@@ -118,54 +114,60 @@ bool StartServiceEasy(WCHAR* name)
 }
 
 
-bool isConnection(char* pHostName)
+bool isConnection(wstring& wsHostName)
 {
-     WORD ver = MAKEWORD(2,2);
-    WSADATA wsaData;
-    int retVal=0;
- 
-    WSAStartup(ver,(LPWSADATA)&wsaData);
- 
-    LPHOSTENT hostEnt;
- 
-    hostEnt = gethostbyname(pHostName);
- 
-    if(!hostEnt)
-    {
-        printf("Unable to collect gethostbyname\n");
-        WSACleanup();
-        return false;
-    }
- 
-    //Создаем сокет
-    SOCKET clientSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
- 
-    if(clientSock == SOCKET_ERROR)
-    {
-        printf("Unable to create socket\n");
-        WSACleanup();
-        return false;
-    }
- 
-    SOCKADDR_IN serverInfo;
- 
-    serverInfo.sin_family = PF_INET;
-    serverInfo.sin_addr = *((LPIN_ADDR)*hostEnt->h_addr_list);
-    serverInfo.sin_port = htons(80);
- 
-    retVal=connect(clientSock,(LPSOCKADDR)&serverInfo, sizeof(serverInfo));
-    if (retVal == SOCKET_ERROR) {
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	int retVal = 0;
+
+	WSAStartup(ver, (LPWSADATA)&wsaData);
+
+	LPHOSTENT hostEnt;
+
+	int resLen = wcstombs(0, wsHostName.c_str(), 0);
+	char* pHostName = new char[resLen + 1];
+	std::wcstombs(pHostName, wsHostName.c_str(), resLen);
+
+	hostEnt = gethostbyname(pHostName);
+
+	delete[] pHostName;
+
+	if (!hostEnt)
+	{
+		printf("Unable to collect gethostbyname\n");
+		WSACleanup();
+		return false;
+	}
+
+	//Создаем сокет
+	SOCKET clientSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (clientSock == SOCKET_ERROR)
+	{
+		printf("Unable to create socket\n");
+		WSACleanup();
+		return false;
+	}
+
+	SOCKADDR_IN serverInfo;
+
+	serverInfo.sin_family = PF_INET;
+	serverInfo.sin_addr = *((LPIN_ADDR)*hostEnt->h_addr_list);
+	serverInfo.sin_port = htons(80);
+
+	retVal = connect(clientSock, (LPSOCKADDR)&serverInfo, sizeof(serverInfo));
+	if (retVal == SOCKET_ERROR) {
 		WSACleanup();
 		return false;
 	};
-	
-	WSACleanup();	
-	
+
+	WSACleanup();
+
 	return true;
 
 }
 
-bool exec(WCHAR* cmd) 
+bool exec(wstring& cmd)
 {
 	PROCESS_INFORMATION pi;
 	ZeroMemory(&pi, sizeof(pi));
@@ -173,11 +175,11 @@ bool exec(WCHAR* cmd)
 	STARTUPINFO si;
 	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
-	
-	wchar_t cmdline[ MAX_PATH + 50 ];
-	memcpy(cmdline, cmd, (wcslen(cmd) + 1)*sizeof(wchar_t));
-	
-	if(CreateProcess(NULL, cmdline, NULL, NULL, FALSE,  NORMAL_PRIORITY_CLASS, NULL, NULL, &si , &pi))
+
+	wchar_t cmdline[MAX_PATH + 50];
+	memcpy(cmdline, cmd.c_str(), (cmd.length() + 1)*sizeof(wchar_t));
+
+	if (CreateProcess(NULL, cmdline, NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi))
 	{
 		CloseHandle(pi.hThread);
 		CloseHandle(pi.hProcess);
@@ -194,33 +196,63 @@ bool exec(WCHAR* cmd)
 
 
 int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR lpCmdLine,
-  int nCmdShow
-)
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+	)
 {
-	// if there is "s" parametr in comman line, 
+
+	wstring wsServiceName = L"KVPNCSvc"; // Имя службы Керио
+	wstring wsPathToRDP; // Путь к файлу настроек подключения RDP
+	wstring wsPathToVPN; //Путь к клиенту
+	wstring wsServerName;// Имя удаленного сервера, к которому идет подключение
+
+	// if there is "s" parametr in command line, 
 	// then it just needs to start "KVPNCSvc" service and nothing more
 	if (*lpCmdLine == 's')
 	{
-		if (!StartServiceEasy((WCHAR*)pServiceName)) return -1;
+		if (!StartServiceEasy(wsServiceName.c_str())) return -1;
 		return 0;
 	}
 
-	// if we already have connection to a server, we can open a remote desktop 
-	if (isConnection((char*)pServerName)) 
+	int numArgs = 0;
+
+	WCHAR** args = CommandLineToArgvW(GetCommandLine(), &numArgs);
+
+	if (numArgs < 3) {
+
+		return -1;
+	}
+
+	std::wstring arg;
+
+	for (int i=0; i < numArgs; i++)
 	{
-		exec((WCHAR*)pPathToRDP);
+		arg = args[i];
+
+		if (arg.substr(0, 4).compare(L"-RDP")==0)
+			wsPathToRDP = arg.substr(4, wstring::npos).c_str();
+		else if (arg.substr(0, 4).compare(L"-VPN")==0)
+			wsPathToVPN = arg.substr(4, wstring::npos).c_str();
+		else if (arg.substr(0, 4).compare(L"-SRV")==0) 
+			wsServerName = arg.substr(4, wstring::npos).c_str();
+
+	}
+
+	// if we already have connection to a server, we can open a remote desktop 
+	if (isConnection(wsServerName))
+	{
+		exec(wsPathToRDP);
 		return 0;
 	}
-    
+
 	// if "KVPNCSvc" service is not started yet, we are going to start it now
 	// by launching our application with administration privileges
-	if (!isServiceStarted((WCHAR*)pServiceName))
+	if (!isServiceStarted(wsServiceName.c_str()))
 	{
 		WCHAR pAppPath[MAX_PATH];
-		GetModuleFileName( NULL, pAppPath, MAX_PATH);
+		GetModuleFileName(NULL, pAppPath, MAX_PATH);
 		ShellExecute(
 			NULL,
 			L"runas",
@@ -230,39 +262,39 @@ int CALLBACK WinMain(
 			SW_SHOW);
 	};
 
-HWND hKerioMainWindow = NULL;
+	HWND hKerioMainWindow = NULL;
 
-int counter = 0;
+	int counter = 0;
 
-if (!exec((WCHAR*)pPathToVPN)) return -1;
+	if (!exec(wsPathToVPN)) return -1;
 
-// waiting till we could get focused window of Kerio VPN Client
-while (true)
-{
-	counter++;
-	Sleep(100); // check for focused window every 100 milliseconds
-	hKerioMainWindow = FindWindow(NULL, L"Kerio VPN Client");
-	if (((hKerioMainWindow != NULL)&&(GetForegroundWindow() == hKerioMainWindow))||(counter > 30)) break;
-}
-
-if (hKerioMainWindow != NULL)
-{
-	// emulate press of Enter key to press connection button
-	SendMessage(hKerioMainWindow, WM_KEYDOWN, VK_RETURN, 0);
-}
-
-while (true)
-{
-	Sleep(1000);
-	if (isConnection((char*)pServerName)) 
+	// waiting till we could get focused window of Kerio VPN Client
+	while (true)
 	{
-		exec((WCHAR*)pPathToRDP);
-		return 0;
+		counter++;
+		Sleep(100); // check for focused window every 100 milliseconds
+		hKerioMainWindow = FindWindow(NULL, L"Kerio VPN Client");
+		if (((hKerioMainWindow != NULL) && (GetForegroundWindow() == hKerioMainWindow)) || (counter > 30)) break;
 	}
-	counter++;
-	if (counter == 20) {
-		return -1;
+
+	if (hKerioMainWindow != NULL)
+	{
+		// emulate press of Enter key to press connection button
+		SendMessage(hKerioMainWindow, WM_KEYDOWN, VK_RETURN, 0);
 	}
-}
+
+	while (true)
+	{
+		Sleep(1000);
+		if (isConnection(wsServerName))
+		{
+			exec(wsPathToRDP);
+			return 0;
+		}
+		counter++;
+		if (counter == 10) {
+			return -1;
+		}
+	}
 	return 0;
 }
